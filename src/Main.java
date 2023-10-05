74b8a35576a74b8112d188b64e7ff6b14b0e9e56
After001 <arthurbaltarwork@gmail.com>
2023-10-04 21:32:38 -0300

    Version04102023

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+
     public static void main(String[] args) throws Exception {
 
         // Criação do Tabuleiro de Xadrez
@@ -10,7 +11,42 @@ public class Main {
             }
         }
 
+        /*
+         * Criação da váriavel "queen";
+         * A primeira se encontra numa posição fixa.
+         */
+        int queen = 1;
+        tabuleiro[0][0] = queen;
 
+    }
 
+    public static boolean verificacao(int[][] tabuleiro, int linha, int coluna) {
+        // Verificar coluna
+        for (int i = 0; i < linha; i++) {
+            if (tabuleiro[i][coluna] != 0) {
+                return false;
+            }
+        }
+
+        // Verificar diagonal superior esquerda
+        for (int i = linha, j = coluna; i >= 0 && j >= 0; i--, j--) {
+            if (tabuleiro[i][j] != 0) {
+                return false;
+            }
+        }
+
+        // Verificar diagonal superior direita
+        for (int i = linha, j = coluna; i >= 0 && j < 8; i--, j++) {
+            if (tabuleiro[i][j] != 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void backTracking() {
+
+        
     }
 }
--- src/oitoRainhas.java
+++
@@ -1,7 +0,0 @@
-public class oitoRainhas {
-
-    public static void verificacao(int[][] tabuleiro, int linha, int coluna) {
-
-    }
-
-}
